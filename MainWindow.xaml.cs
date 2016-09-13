5cae841d0e035f7b77fd977b438fea47056e7ebf
mike stedman <github@ravuya.com>
2016-09-13 14:24:02 -0600

    Now has excellent framerate thanks to InteropBitmap

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -9,10 +10,12 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WinFormsBufferingDemo.Utils;
 
 namespace WinFormsBufferingDemo
 {
@@ -22,35 +25,33 @@ namespace WinFormsBufferingDemo
     public partial class MainWindow : Window
     {
         private MemoryStream _memoryStream;
+        private System.Drawing.Bitmap _legacyRenderTarget;
 
         public MainWindow()
         {
             InitializeComponent();
 
-            _memoryStream = new MemoryStream();
+            //_memoryStream = new MemoryStream();
 
             _winformsControl.Paint += (s, e) =>
             {
-                // Doo doo doo
-                using (var bitmap = new System.Drawing.Bitmap(_winformsControl.Width, _winformsControl.Height))
+                // WinForms finished painting, the control is ready to 'screenshot'
+
+                if(_legacyRenderTarget == null)
                 {
-                    _winformsControl.DrawToBitmap(bitmap, _winformsControl.Bounds);
+                    // Keep the same backbuffer forever
+                    _legacyRenderTarget = new System.Drawing.Bitmap(_winformsControl.Width, _winformsControl.Height);
+                }
 
-                    bitmap.Save(_memoryStream, System.Drawing.Imaging.ImageFormat.Bmp);
-                    _memoryStream.Position = 0; // seek back                    
+                // Screenshot the WinForms control into a WinForms bitmap
+                _winformsControl.DrawToBitmap(_legacyRenderTarget, _winformsControl.Bounds);
 
-                    var wpfBitmap = new BitmapImage();
-                    wpfBitmap.BeginInit();
-
-                    wpfBitmap.CacheOption = BitmapCacheOption.OnLoad;
-                    wpfBitmap.StreamSource = _memoryStream;
-
-                    wpfBitmap.EndInit();
-
-                    // Put it on the back buffer
-                    _buf.Source = wpfBitmap;
-
-                    _memoryStream.Position = 0;
+                // Wire up WPF to consume that WinForms bitmap using Interop Bitmap.
+                // Consume that backbuffer forever
+                //(_buf.Source as InteropBitmap).Dispose();
+                using (var hBitmap = new HBitmapWrapper(_legacyRenderTarget))
+                {
+                    _buf.Source = (InteropBitmap)Imaging.CreateBitmapSourceFromHBitmap(hBitmap, IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
                 }
             };
         }
---
+++ Utils/HBitmapWrapper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace WinFormsBufferingDemo.Utils
+{
+    /// <summary>
+    /// RAII wrapper for getting and disposing of a handle from a bitmap
+    /// </summary>
+    internal class HBitmapWrapper : IDisposable
+    {
+        private IntPtr _hBitmap;
+
+        public HBitmapWrapper(System.Drawing.Bitmap source)
+        {
+            _hBitmap = source.GetHbitmap();
+        }
+
+        public void Dispose()
+        {
+            DeleteObject(_hBitmap);
+        }
+
+        public static implicit operator IntPtr(HBitmapWrapper wrapper)
+        {
+            return wrapper._hBitmap;
+        }
+
+        [DllImport("gdi32")]
+        static extern int DeleteObject(IntPtr o);
+    }
+}
